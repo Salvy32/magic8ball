000974411d6dd40de9149a8674155772fb35d610
Salvador Servin <servin52@msn.com>
2024-01-19 16:25:57 -0800

    magic eight ball

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ask the magic eight ball any question in your mind and click the button to get a response.
---
+++ magic8ball/js/main.js
@@ -0,0 +1,31 @@
+document.querySelector("button").addEventListener("click", fortune);
+
+function fortune() {
+  let randomNumber = Math.floor(Math.random() * 8);
+  let eightBall = "";
+  switch (randomNumber) {
+    case 0:
+      eightBall = "It is certain";
+      break;
+    case 1:
+      eightBall = "It is decidedly so";
+      break;
+    case 2:
+      eightBall = "Hazy try again";
+      break;
+    case 3:
+      eightBall = "Cannot predict now";
+      break;
+    case 4:
+      eightBall = "Do not count on it";
+      break;
+    case 5:
+      eightBall = "My sources say no";
+      break;
+    case 6:
+      eightBall = "Outlook not so good";
+    case 7:
+      eightBall = "Signs point to yes";
+  }
+  document.querySelector("h2").innerText = `${eightBall}`;
+}
